1245594804a051bc934c327d728521954ee7c7ea
Derek Kinzo <derekkinzo@gmail.com>
2017-06-03 12:16:50 -0600

    Update project settings and main view to always grow

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,13 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Flash Anzan");
+        primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
