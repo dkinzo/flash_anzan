8ef75cc9be5942f810e3cfbfcf02606348b117b2
Derek Kinzo <derekkinzo@gmail.com>
2017-06-03 17:51:07 -0600

    Added fxml object names and started implementing comboBox range initialization

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,9 +1,42 @@
 package main;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class Controller {
 
-    public void initialize(){
+    @FXML
+    private ComboBox comboRange = new ComboBox();
 
+    /**
+     * @Constant Array containing max values of random number
+     */
+    final private int maxValue[] = {
+            9,
+            99,
+            999,
+            9999,
+            99999
+    };
+
+//    //Buttons
+//    @FXML
+//    private Button btnDivision;
+//    @FXML
+//    private Button btnPlus;
+//    @FXML
+//    private Button btnMinus;
+//    @FXML
+//    private Button btnMultiplication;
+
+
+    public void initialize(){
+        final String comboBoxMessage = "1 - ";
+        //Initialize range comboBox
+        comboRange.getItems().add(comboBoxMessage);
 
     }
 }
