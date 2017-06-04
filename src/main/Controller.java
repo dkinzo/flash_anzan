e4c5f77c0123ddec07ec08558c445f81ac7a1133
Derek Kinzo <derekkinzo@gmail.com>
2017-06-03 23:57:32 -0600

    Implemented getRandomNumber Function

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -6,16 +6,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Controller {
 
     @FXML
     private ComboBox<String> comboRange = new ComboBox<>();
-
-    @FXML
-    private ImageView playButton = new ImageView();
-
-    @FXML
-    private Label randomNumbers = new Label();
 
     /**
      * Array containing max values of random number
@@ -27,6 +24,43 @@ public class Controller {
             9999,
             99999
     };
+
+    @FXML
+    private ImageView playButton = new ImageView();
+
+    @FXML
+    private Label labelRandomNumber = new Label();
+
+    private Random randGenerator = new Random();
+
+    /**
+     * Generate random number according to range value specified by comboBox
+     * @return random number generated
+     */
+    private int getRandomNumber(){
+        int rangeIndex = 0; //Helper variable to store range value
+        try {
+            //check range value from comboBox
+            rangeIndex = comboRange.getSelectionModel().getSelectedIndex();
+
+            //generate random number
+            //Add 1 to value since generator may return value 0
+            return (randGenerator.nextInt(maxValue[rangeIndex]) + 1);
+
+        }
+        catch (IndexOutOfBoundsException e){
+            //If out of bounds or range hasn't been selected. Select 1-9 as default
+            comboRange.getSelectionModel().select(0);
+            rangeIndex = 0;
+
+            //generate random number
+            //Add 1 to value since generator may return value 0
+            return (randGenerator.nextInt(maxValue[rangeIndex]) + 1);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
 
     public void initialize(){
 
@@ -47,12 +81,17 @@ public class Controller {
     }
 
     public void playOnClick(){
+        int _randomNumber;
+
         //make play button invisible
         playButton.setVisible(false);
 
-        //display number on text area
-        randomNumbers.setText("5");
-
+        //Sanity check label is visible
+        labelRandomNumber.setVisible(true);
+        //Generate random number according to range
+        _randomNumber = getRandomNumber();
+        //Display number on text area
+        labelRandomNumber.setText(Integer.toString(_randomNumber));
     }
 
 }
